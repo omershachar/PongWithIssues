f3b822ad558da6fed901c858c774ea3c2ca622a6
omer <omershachar98@icloud.com>
2025-06-15 01:31:14 +0300

    updated board size and net, removed ball's green arc

--- BETA/main.py
+++ BETA/main.py
@@ -7,7 +7,7 @@ pygame.display.set_caption("PongWithIssues BETA")
 # Constants
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-WIDTH, HEIGHT = 700, 500
+WIDTH, HEIGHT = 900, 600  # Enlarged board
 PADDLE_WIDTH, PADDLE_HEIGHT = 20, 100
 BALL_RADIUS = 7
 WINNING_SCORE = 3
@@ -16,25 +16,28 @@ FPS = 60
 SCORE_FONT = pygame.font.SysFont("comicsans", 50)
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 
-MAX_DEFLECTION_SPEED = 5  # max vertical speed added from paddle hit
+MAX_DEFLECTION_SPEED = 5
 SPIN_FACTOR = 0.3
 
 def handle_ball_collision(ball, left_paddle, right_paddle):
     if ball.y + ball.radius >= HEIGHT or ball.y - ball.radius <= 0:
-        ball.y_vel *= -1  # bounce off top/bottom
+        ball.y_vel *= -1
 
-    if ball.x_vel < 0:  # moving left
+    if ball.x_vel < 0:
         if left_paddle.y <= ball.y <= left_paddle.y + left_paddle.height:
             if ball.x - ball.radius <= left_paddle.x + left_paddle.width:
                 relative_velocity = ball.y_vel - left_paddle.y_vel
                 impulse = 2 * ball.mass * relative_velocity
                 ball.apply_impulse(0, -impulse)
-                ball.spin = left_paddle.y_vel * 0.5
 
-                # recoil effect on paddle
+                if left_paddle.power_spin:
+                    ball.spin = left_paddle.y_vel * 2
+                    left_paddle.power_spin = False
+                else:
+                    ball.spin = left_paddle.y_vel * 0.5
+
                 left_paddle.apply_impulse(0, -impulse * 0.1)
 
-                # angle deflection logic
                 middle_y = left_paddle.y + left_paddle.height / 2
                 offset = ball.y - middle_y
                 normalized_offset = offset / (left_paddle.height / 2)
@@ -42,18 +45,21 @@ def handle_ball_collision(ball, left_paddle, right_paddle):
 
                 ball.x_vel *= -1
 
-    elif ball.x_vel > 0:  # moving right
+    elif ball.x_vel > 0:
         if right_paddle.y <= ball.y <= right_paddle.y + right_paddle.height:
             if ball.x + ball.radius >= right_paddle.x:
                 relative_velocity = ball.y_vel - right_paddle.y_vel
                 impulse = 2 * ball.mass * relative_velocity
                 ball.apply_impulse(0, -impulse)
-                ball.spin = right_paddle.y_vel * 0.5
 
-                # recoil effect on paddle
+                if right_paddle.power_spin:
+                    ball.spin = right_paddle.y_vel * 2
+                    right_paddle.power_spin = False
+                else:
+                    ball.spin = right_paddle.y_vel * 0.5
+
                 right_paddle.apply_impulse(0, -impulse * 0.1)
 
-                # angle deflection logic
                 middle_y = right_paddle.y + right_paddle.height / 2
                 offset = ball.y - middle_y
                 normalized_offset = offset / (right_paddle.height / 2)
@@ -61,8 +67,12 @@ def handle_ball_collision(ball, left_paddle, right_paddle):
 
                 ball.x_vel *= -1
 
-
 def handle_paddle_movement(keys, left_paddle, right_paddle):
+    if keys[pygame.K_q]:
+        left_paddle.power_spin = True
+    if keys[pygame.K_RSHIFT]:
+        right_paddle.power_spin = True
+
     if keys[pygame.K_w]:
         left_paddle.accelerate(up=True)
     if keys[pygame.K_s]:
@@ -72,7 +82,6 @@ def handle_paddle_movement(keys, left_paddle, right_paddle):
         right_paddle.accelerate(up=True)
     if keys[pygame.K_DOWN]:
         right_paddle.accelerate(up=False)
-
 
 def draw(win, paddles, ball, left_score, right_score):
     win.fill(BLACK)
@@ -85,13 +94,12 @@ def draw(win, paddles, ball, left_score, right_score):
     for paddle in paddles:
         paddle.draw(win)
 
-    for i in range(10, HEIGHT, HEIGHT // 20):
-        if i % 2 == 0:
-            pygame.draw.rect(win, WHITE, (WIDTH // 2 - 5, i, 10, HEIGHT // 20))
+    # Draw a net with a mesh-like effect
+    for i in range(10, HEIGHT, HEIGHT // 25):
+        pygame.draw.rect(win, WHITE, (WIDTH // 2 - 2, i, 4, HEIGHT // 50))
 
     ball.draw(win)
     pygame.display.update()
-
 
 def main():
     clock = pygame.time.Clock()
@@ -143,7 +151,6 @@ def main():
             right_score = 0
 
     pygame.quit()
-
 
 if __name__ == '__main__':
     main()
--- BETA/physics.py
+++ BETA/physics.py
@@ -13,20 +13,18 @@ class PhysicsObject:
         self.x_vel += impulse_x / self.mass
         self.y_vel += impulse_y / self.mass
 
-
 class Ball(PhysicsObject):
     def __init__(self, x, y, radius, mass=1):
         super().__init__(mass, x, y, x_vel=5, y_vel=0)
         self.spin = 0
-        self.trail = []  # stores past positions
-        self.max_trail = 10  # max trail length
+        self.trail = []
+        self.max_trail = 10
         self.radius = radius
         self.original_x = x
         self.original_y = y
 
     def draw(self, win):
         import pygame
-        # draw trail
         for i, pos in enumerate(self.trail):
             alpha = max(50, 255 - i * 20)
             radius = max(1, self.radius - i // 2)
@@ -34,7 +32,6 @@ class Ball(PhysicsObject):
             pygame.draw.circle(surface, (255, 255, 255, alpha), (radius, radius), radius)
             win.blit(surface, (pos[0] - radius, pos[1] - radius))
 
-        # draw main ball
         pygame.draw.circle(win, (255, 255, 255), (int(self.x), int(self.y)), self.radius)
 
         if abs(self.spin) > 0.2:
@@ -43,34 +40,33 @@ class Ball(PhysicsObject):
             arc_rect = pygame.Rect(int(self.x - self.radius), int(self.y - self.radius), self.radius * 2, self.radius * 2)
             pygame.draw.arc(win, (0, 255, 0), arc_rect, direction, direction + math.pi, 2)
 
-
     def move(self):
-        self.y_vel += self.spin * 0.1  # Magnus effect curve
-        self.spin *= 0.96 
+        self.y_vel += self.spin * 0.1
+        self.spin *= 0.96
         self.x += self.x_vel
         self.y += self.y_vel
 
     def update(self):
-        # Add current position to trail
         self.trail.insert(0, (int(self.x), int(self.y)))
         if len(self.trail) > self.max_trail:
             self.trail.pop()
-        self.move()  # reuse existing move
+        self.move()
 
     def reset(self):
         self.x = self.original_x
         self.y = self.original_y
         self.x_vel *= -1
         self.y_vel = 0.9
+        self.spin = 0
 
     @property
     def VEL(self):
         return (self.x_vel ** 2 + self.y_vel ** 2) ** 0.5
 
-
 class Paddle(PhysicsObject):
     def __init__(self, x, y, width, height, mass=1000):
         super().__init__(mass, x, y)
+        self.power_spin = False
         self.original_x = x
         self.original_y = y
         self.width = width
@@ -89,7 +85,6 @@ class Paddle(PhysicsObject):
         else:
             self.y_vel += self.ACC
 
-        # Clamp velocity to max speed
         if self.y_vel > self.MAX_VEL:
             self.y_vel = self.MAX_VEL
         elif self.y_vel < -self.MAX_VEL:
@@ -97,17 +92,16 @@ class Paddle(PhysicsObject):
 
     def update(self):
         self.y += self.y_vel
-        self.y_vel *= 0.85  # decay / friction
-        # Clamp to screen bounds
+        self.y_vel *= 0.85
         if self.y < 0:
             self.y = 0
             self.y_vel = 0
-        elif self.y + self.height > 500:  # HEIGHT
+        elif self.y + self.height > 500:
             self.y = 500 - self.height
             self.y_vel = 0
-
 
     def reset(self):
         self.x = self.original_x
         self.y = self.original_y
         self.y_vel = 0
+        self.power_spin = False
