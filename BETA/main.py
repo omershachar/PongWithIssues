e1eb0c23e91f59c00d76b22a452cf087fb04efea
omer <omershachar98@icloud.com>
2025-06-15 13:42:04 +0300

    insane bug fixes

--- BETA/main.py
+++ BETA/main.py
@@ -7,7 +7,7 @@ pygame.display.set_caption("PongWithIssues BETA")
 # Constants
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-WIDTH, HEIGHT = 900, 600  # Enlarged board
+WIDTH, HEIGHT = 700, 500
 PADDLE_WIDTH, PADDLE_HEIGHT = 20, 100
 BALL_RADIUS = 7
 WINNING_SCORE = 3
@@ -16,28 +16,25 @@ FPS = 60
 SCORE_FONT = pygame.font.SysFont("comicsans", 50)
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 
-MAX_DEFLECTION_SPEED = 5
+MAX_DEFLECTION_SPEED = 5  # max vertical speed added from paddle hit
 SPIN_FACTOR = 0.3
 
 def handle_ball_collision(ball, left_paddle, right_paddle):
     if ball.y + ball.radius >= HEIGHT or ball.y - ball.radius <= 0:
-        ball.y_vel *= -1
+        ball.y_vel *= -1  # bounce off top/bottom
 
-    if ball.x_vel < 0:
+    if ball.x_vel < 0:  # moving left
         if left_paddle.y <= ball.y <= left_paddle.y + left_paddle.height:
             if ball.x - ball.radius <= left_paddle.x + left_paddle.width:
                 relative_velocity = ball.y_vel - left_paddle.y_vel
                 impulse = 2 * ball.mass * relative_velocity
                 ball.apply_impulse(0, -impulse)
+                ball.spin = left_paddle.y_vel * 0.5
 
-                if left_paddle.power_spin:
-                    ball.spin = left_paddle.y_vel * 2
-                    left_paddle.power_spin = False
-                else:
-                    ball.spin = left_paddle.y_vel * 0.5
-
+                # recoil effect on paddle
                 left_paddle.apply_impulse(0, -impulse * 0.1)
 
+                # angle deflection logic
                 middle_y = left_paddle.y + left_paddle.height / 2
                 offset = ball.y - middle_y
                 normalized_offset = offset / (left_paddle.height / 2)
@@ -45,21 +42,18 @@ def handle_ball_collision(ball, left_paddle, right_paddle):
 
                 ball.x_vel *= -1
 
-    elif ball.x_vel > 0:
+    elif ball.x_vel > 0:  # moving right
         if right_paddle.y <= ball.y <= right_paddle.y + right_paddle.height:
             if ball.x + ball.radius >= right_paddle.x:
                 relative_velocity = ball.y_vel - right_paddle.y_vel
                 impulse = 2 * ball.mass * relative_velocity
                 ball.apply_impulse(0, -impulse)
+                ball.spin = right_paddle.y_vel * 0.5
 
-                if right_paddle.power_spin:
-                    ball.spin = right_paddle.y_vel * 2
-                    right_paddle.power_spin = False
-                else:
-                    ball.spin = right_paddle.y_vel * 0.5
-
+                # recoil effect on paddle
                 right_paddle.apply_impulse(0, -impulse * 0.1)
 
+                # angle deflection logic
                 middle_y = right_paddle.y + right_paddle.height / 2
                 offset = ball.y - middle_y
                 normalized_offset = offset / (right_paddle.height / 2)
@@ -67,12 +61,8 @@ def handle_ball_collision(ball, left_paddle, right_paddle):
 
                 ball.x_vel *= -1
 
-def handle_paddle_movement(keys, left_paddle, right_paddle):
-    if keys[pygame.K_q]:
-        left_paddle.power_spin = True
-    if keys[pygame.K_RSHIFT]:
-        right_paddle.power_spin = True
 
+def handle_paddle_movement(keys, left_paddle, right_paddle):
     if keys[pygame.K_w]:
         left_paddle.accelerate(up=True)
     if keys[pygame.K_s]:
@@ -82,6 +72,7 @@ def handle_paddle_movement(keys, left_paddle, right_paddle):
         right_paddle.accelerate(up=True)
     if keys[pygame.K_DOWN]:
         right_paddle.accelerate(up=False)
+
 
 def draw(win, paddles, ball, left_score, right_score):
     win.fill(BLACK)
@@ -94,12 +85,13 @@ def draw(win, paddles, ball, left_score, right_score):
     for paddle in paddles:
         paddle.draw(win)
 
-    # Draw a net with a mesh-like effect
-    for i in range(10, HEIGHT, HEIGHT // 25):
-        pygame.draw.rect(win, WHITE, (WIDTH // 2 - 2, i, 4, HEIGHT // 50))
+    for i in range(10, HEIGHT, HEIGHT // 20):
+        if i % 2 == 0:
+            pygame.draw.rect(win, WHITE, (WIDTH // 2 - 5, i, 10, HEIGHT // 20))
 
     ball.draw(win)
     pygame.display.update()
+
 
 def main():
     clock = pygame.time.Clock()
@@ -151,6 +143,7 @@ def main():
             right_score = 0
 
     pygame.quit()
+
 
 if __name__ == '__main__':
     main()
