8bb5b76f0ce2af999385582b11f1100e26a52195
omer <omershachar98@icloud.com>
2025-06-16 22:37:57 +0300

    added new design for menu window

--- classic/classicPong.py
+++ classic/classicPong.py
@@ -3,7 +3,7 @@ classicPong.py -- Main file contain only essentials and activation commands.
 """
 
 import pygame
-from pong.constants import BLACK, WHITE, LIGHT_GREY, PURPLE, WIDTH, HEIGHT, MIDDLE_BOARD, FPS, WINNING_SCORE
+from pong.constants import BLACK, WHITE, LIGHT_GREY, PURPLE, LIGHT_PURPLE, WIDTH, HEIGHT, MIDDLE_BOARD, FPS, WINNING_SCORE
 from pong.constants import PADDLE_WIDTH, PADDLE_HEIGHT, ORIGINAL_LEFT_PADDLE_POS, ORIGINAL_RIGHT_PADDLE_POS, PADDLE_DEFAULT_VEL
 from pong.constants import BALL_RADIUS, BALL_DEFAULT_VEL
 from pong.paddle import Paddle
@@ -23,19 +23,18 @@ TITLE_FONT = pygame.font.SysFont("comicsans", 90)
 FPS = 60 # Frame Per Second
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 
-def draw_menu(win):
+def draw_menu(WIN):
     WIN.fill(BLACK)
     title = TITLE_FONT.render("PONG!", True, PURPLE)
     WIN.blit(title, (WIDTH // 2 - title.get_width() // 2, HEIGHT // 3))
 
-    subtitle = SMALL_FONT.render("A project that probably works. Sometimes. Maybe", True, LIGHT_GREY)
+    subtitle = SMALL_FONT.render("A project that probably works. Sometimes. Maybe", True, LIGHT_PURPLE)
     WIN.blit(subtitle, (WIDTH // 2 - subtitle.get_width() // 2, HEIGHT // 3 + 90))
-    sub2 = SMALL_FONT.render("It compiles. That's enough.", True, LIGHT_GREY)
+    sub2 = SMALL_FONT.render("It compiles. That's enough.", True, LIGHT_PURPLE)
     WIN.blit(sub2, (WIDTH // 2 - sub2.get_width() // 2, HEIGHT // 3 + 135))
 
     prompt = SMALL_FONT.render("Press [SPACE] to start", True, PURPLE)
     WIN.blit(prompt, (WIDTH // 2 - prompt.get_width() // 2, HEIGHT // 2 + 120))
-
     pygame.display.update()
 
 def main():
@@ -44,9 +43,11 @@ def main():
     state = MENU
 
     # Initializing game the objects
-    left_paddle = Paddle(ORIGINAL_LEFT_PADDLE_POS[0], ORIGINAL_LEFT_PADDLE_POS[1], PADDLE_WIDTH, PADDLE_HEIGHT, WHITE, PADDLE_DEFAULT_VEL)
-    right_paddle = Paddle(ORIGINAL_RIGHT_PADDLE_POS[0], ORIGINAL_RIGHT_PADDLE_POS[1], PADDLE_WIDTH, PADDLE_HEIGHT, WHITE, PADDLE_DEFAULT_VEL)
-    ball = Ball(MIDDLE_BOARD[0], MIDDLE_BOARD[1], BALL_RADIUS, WHITE, BALL_DEFAULT_VEL[0], BALL_DEFAULT_VEL[1])
+    left_paddle = Paddle(ORIGINAL_LEFT_PADDLE_POS[0], ORIGINAL_LEFT_PADDLE_POS[1], PADDLE_WIDTH, PADDLE_HEIGHT, LIGHT_PURPLE, PADDLE_DEFAULT_VEL)
+    right_paddle = Paddle(ORIGINAL_RIGHT_PADDLE_POS[0], ORIGINAL_RIGHT_PADDLE_POS[1], PADDLE_WIDTH, PADDLE_HEIGHT, LIGHT_PURPLE, PADDLE_DEFAULT_VEL)
+    ball = Ball(MIDDLE_BOARD[0], MIDDLE_BOARD[1], BALL_RADIUS, LIGHT_PURPLE, BALL_DEFAULT_VEL[0], BALL_DEFAULT_VEL[1])
+
+    ball2 = Ball(MIDDLE_BOARD[0], MIDDLE_BOARD[1], BALL_RADIUS, WHITE, BALL_DEFAULT_VEL[0], 4)
 
     left_score = 0
     right_score = 0
@@ -63,6 +64,12 @@ def main():
 
         if state == MENU:
             draw_menu(WIN)
+            Keys = pygame.key.get_pressed() # Getting the pressed keys of the players
+            ball2.draw(WIN)
+            ball2.move()
+            # handle_ball_collision(ball2, left_paddle, right_paddle, HEIGHT)
+            ball2.bounce_box(WIDTH,HEIGHT)
+            pygame.display.update()
             continue
 
         Keys = pygame.key.get_pressed() # Getting the pressed keys of the players
@@ -94,7 +101,7 @@ def main():
             win_text = "Right Player Won!"
         
         if won:
-            text = SCORE_FONT.render(win_text, 1, WHITE)
+            text = SCORE_FONT.render(win_text, 1, PURPLE)
             WIN.blit(text, (WIDTH // 2 - text.get_width() // 2, HEIGHT // 2 - text.get_height() // 2))
             pygame.display.update()
             pygame.time.delay(3000)
