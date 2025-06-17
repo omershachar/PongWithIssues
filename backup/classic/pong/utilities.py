4533ff608f277284869ca77d0d50b7a24325b751
omer <omershachar98@icloud.com>
2025-06-17 14:05:35 +0300

    backup working versions for safety

---
+++ backup/classic/pong/utilities.py
@@ -0,0 +1,26 @@
+"""
+utilities.py -- File for containing all environment and design methods for the game.
+"""
+
+import pygame
+import numpy as np
+from pong.constants import BLACK, DARK_GREY, GREY, LIGHT_GREY, WHITE, PURPLE, LIGHT_PURPLE, WIDTH, HEIGHT, FPS, WINNING_SCORE
+
+# Function for drawing the board
+def draw(win, paddles, ball, left_score, right_score, score_font):
+    win.fill(BLACK)
+
+    left_score_text = score_font.render(f"{left_score}", 1, LIGHT_PURPLE)
+    right_score_text = score_font.render(f"{right_score}", 1, LIGHT_PURPLE)
+    win.blit(left_score_text, (WIDTH//4 - left_score_text.get_width()//2, 20))
+    win.blit(right_score_text, (WIDTH * (3/4) - right_score_text.get_width()//2, 20))
+
+    for paddle in paddles:
+        paddle.draw(win)
+
+    for i in range(10, HEIGHT-10, HEIGHT//20): # Drawing the net
+        if i % 2 == 1:
+            continue
+        pygame.draw.rect(win, LIGHT_PURPLE, (WIDTH//2, i, 3, WIDTH//70))
+    ball.draw(win)
+    pygame.display.update()
