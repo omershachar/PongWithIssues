f967fecbdd024780353ea94603fb6fdbf5a5402b
omer <omershachar98@icloud.com>
2025-06-17 19:10:29 +0300

    added fonts style as a tuple

--- pong/constants.py
+++ pong/constants.py
@@ -13,8 +13,16 @@ WHITE = (255, 255, 255)
 PURPLE = (122, 118, 229)
 LIGHT_PURPLE = (185, 183, 232)
 
+# Fonts (in a tuple)
+FONT_SMALL = ("comicsans", 25)
+FONT_DEFAULT = ("comicsans", 40)
+FONT_LARGE = ("comicsans", 50)
+FONT_BIG = ("comicsans", 65)
+FONT_TITLE = ("comicsans", 90)
+
 # Sizes and coordinates
 WIDTH, HEIGHT = 800, 800 # Board size
+WIDTH_LAUNCHER, HEIGHT_LAUNCHER = 600, 400
 MARGIN = 10
 MIDDLE_BOARD = np.array([WIDTH // 2, HEIGHT // 2], dtype=int)
 
