9d056a31d46b1ba6da036d0abaa6841b02e4f69b
omer <omershachar98@icloud.com>
2025-06-18 04:47:07 +0300

    changed paddle constructor for pongception

--- pong/paddle.py
+++ pong/paddle.py
@@ -5,42 +5,41 @@ paddle.py -- Class for storing paddles attributes and methods
 import pygame
 import numpy as np
 from pong.physics_object import PhysicsObject
+from pong.constants import *
 
 class Paddle(PhysicsObject):
-    def __init__(self, x, y, width, height, mass=1000):
-        super().__init__(pos=(x, y), mass=mass, vel=(0, 0))
+    def __init__(self, x, y, width, height, color=LIGHT_PURPLE):
+        super().__init__(pos=(x, y), mass=1, vel=(0, 0))
         self.original_pos = np.array([x, y], dtype=float)
         self.width = width
         self.height = height
-        # self.acc = np.array([0.0, 0.0], dtype=float) # Already initialized in base class!
-        self.MAX_VEL = 7
-        self.ACC = 1.2
-
+        self.color = color
+        
     def draw(self, win):
-        import pygame
-        pygame.draw.rect(win, (255, 255, 255), 
-                         (int(self.pos[0]), int(self.pos[1]), self.width, self.height))
+        pygame.draw.rect(win, self.color, (int(self.pos[0]), int(self.pos[1]), self.width, self.height))
 
     def accelerate(self, up=True):
         if up:
-            self.acc[1] -= self.ACC
+            self.acc[:] -= PADDLE_DEFAULT_ACC[:]
         else:
-            self.acc[1] += self.ACC
+            self.acc[:] += PADDLE_DEFAULT_ACC[:]
 
     def update(self):
         # Apply acceleration to velocity, then decay/friction
         self.vel += self.acc
         self.acc[:] = 0
         # Clamp vertical velocity
-        if self.vel[1] > self.MAX_VEL:
-            self.vel[1] = self.MAX_VEL
-        elif self.vel[1] < -self.MAX_VEL:
-            self.vel[1] = -self.MAX_VEL
+        if self.vel[1] > PADDLE_MAX_VEL:
+            self.vel[1] = PADDLE_MAX_VEL
+        elif self.vel[1] < -PADDLE_MAX_VEL:
+            self.vel[1] = -PADDLE_MAX_VEL
         # Update position
         self.pos += self.vel
+        # friction_force = self.mass * GRAVITY * FRICTION_COEFFICIENT * -1
+        # self.apply_force(friction_force)
+        # self.acc *= [0, 1]
         self.vel[1] *= 0.85  # friction/decay only on y
         # Clamp to screen bounds (assuming HEIGHT is global or import)
-        from pong.constants import HEIGHT
         if self.pos[1] < 0:
             self.pos[1] = 0
             self.vel[1] = 0
